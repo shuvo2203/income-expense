95493d03234b17894f42c41a6f5389cc706b5a10
shuvo <mehedihasanshuvo.cse@gmail.com>
2023-11-29 22:50:17 +0600

    transectin backend complete

--- backend/controllers/transectionController.js
+++ backend/controllers/transectionController.js
@@ -3,9 +3,23 @@ const Transection = require('../models/transectionModel');
 
 
 exports.addTransection=async(req, res)=>{
-
+    const{amount,category,description,date} = req.body;
+    const transection = await Transection.create({
+        amount,category,description,date
+    });
+    res.status(200).json({
+        success:true,
+        transection
+    });
 }
 
 exports.getAllTransection=async(req, res)=>{
-    
+    const allTransection = await Transection.find({});
+    if(!allTransection){
+        res.status(400).json('Transection not found');
+    }
+    res.status(200).json({
+        success:true,
+        allTransection
+    });
 }
--- backend/routes/transectionRoute.js
+++ backend/routes/transectionRoute.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const {
+    addTransection,
+    getAllTransection
+} = require('../controllers/transectionController');
 const router = express.Router();
 
-router.route()
+router.route('/add-transection').post(addTransection);
+router.route('/get-transection').get(getAllTransection);
 
 module.exports = router;
